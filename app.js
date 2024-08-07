f18c60f0a9099f9d207fa5bb2016036e4f5d44f0
Eric Son <urustin@naver.com>
2024-08-07 17:09:56 +0900

    jenkins 10

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
     cors({
       origin: [
-        'https://luna-client1.vercel.app',
+        'https://luna-client.vercel.app',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
       ],
